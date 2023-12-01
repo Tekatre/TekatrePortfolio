eac70c17d3eacd170bbed2b5fa5e00cc6b752641
Thomas Petit <6poules@gmail.com>
2023-12-01 15:10:04 -0500

    [+] general css thingy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import IMG from "$lib/images/svelte-welcome.png";
+  import stars_banner from "$lib/images/stars.gif";
 </script>
 
 <svelte:head>
@@ -8,15 +8,18 @@
 </svelte:head>
 
 <body>
+  <div class="home-page-banner">
+    <p>Tekatre's Portfolio</p>
+    <!-- <img src={stars_banner} alt="stars" /> -->
+  </div>
   <section>
-    <h1>ART</h1>
-    <section class="thumbnail-display">
-      <div class="thumbnail-frame"></div>
-      <div class="thumbnail-frame"></div>
-      <div class="thumbnail-frame"></div>
-      <div class="thumbnail-frame"></div>
-      <div class="thumbnail-frame"></div>
-    </section>
+    <h1>SOCIALS</h1>
+    <div class="button-list">
+      <button class="social-button mastodon"> Mastodon </button>
+      <button class="social-button discord"> Discord </button>
+      <button class="social-button youtube"> Youtube </button>
+      <button class="social-button github"> GitHub </button>
+    </div>
   </section>
   <section>
     <h1>GAMES</h1>
