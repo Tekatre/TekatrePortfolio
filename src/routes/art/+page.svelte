8e299e0dc7f372ae289e67bd0f7d2db5e9ea6044
Tekatre s <6poules@gmail.com>
2024-01-29 12:38:47 -0500

    dynamic art display

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,10 +1,5 @@
-<script>
-  import IMG1 from "$lib/images/buildings.png";
-  import IMG2 from "$lib/images/goutte.png";
-  import IMG3 from "$lib/images/iso1.png";
-  import IMG4 from "$lib/images/Lamontagneilparé.png";
-  import IMG5 from "$lib/images/logo-CDP.gif";
-  import IMG6 from "$lib/images/pear36-8x.png";
+<script lang="ts">
+  const imageModules = import.meta.glob("$lib/images/artDisplay/*.(png|gif)");
 </script>
 
 <svelte:head>
@@ -15,23 +10,10 @@
 <body>
   <h1>ART</h1>
   <section class="images-mosaic">
-    <div class="image-container">
-      <img class="mosaic-piece" src={IMG1} alt="IMG1" />
-    </div>
-    <div class="image-container">
-      <img class="mosaic-piece" src={IMG2} alt="IMG2" />
-    </div>
-    <div class="image-container">
-      <img class="mosaic-piece" src={IMG3} alt="IMG3" />
-    </div>
-    <div class="image-container">
-      <img class="mosaic-piece" src={IMG6} alt="IMG6" />
-    </div>
-    <div class="image-container">
-      <img class="mosaic-piece" src={IMG4} alt="IMG4" />
-    </div>
-    <div class="image-container">
-      <img class="mosaic-piece" src={IMG5} alt="IMG5" />
-    </div>
+    {#each Object.keys(imageModules) as image}
+      <div class="image-container">
+        <img class="mosaic-piece" src={image} alt="IMG1" />
+      </div>
+    {/each}
   </section>
 </body>
