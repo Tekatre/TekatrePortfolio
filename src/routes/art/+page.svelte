95c776e49d9bc6c3f7c51d55e4fa969976859a53
Tekatre s <6poules@gmail.com>
2024-01-31 18:09:00 -0500

    fix on art section try 2

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
-  const imageModules = import.meta.glob("$lib/images/artDisplay/*.(png|gif)");
-  console.log("imageModules", imageModules);
+  // const imageModules = import.meta.glob("$lib/images/artDisplay/*.(png|gif)");
+  // console.log("imageModules", imageModules);
+
+  export const get = async () => {
+    const allimgsf = import.meta.glob("$lib/images/artDisplay/*.(png|gif)");
+    const iterableImages = Object.entries(allimgsf);
+
+    const allimgs = await Promise.all(
+      iterableImages.map(async ([path, resolver]) => {
+        // gets the resolved asset path
+        const imgPath = await (
+          resolver as () => Promise<{ default: string }>
+        )();
+
+        return {
+          path: imgPath,
+          name: imgPath.default.slice(0, -4).split("/").at(-1),
+        };
+      })
+    );
+
+    return allimgs;
+  };
 </script>
 
 <svelte:head>
@@ -11,11 +32,20 @@
 <body class="surround-div">
   <h1>ART</h1>
   <section class="images-mosaic">
-    {#each Object.keys(imageModules) as image}
+    <!-- {#each Object.keys(imageModules) as image}
       <div class="image-container">
         {@debug image}
         <img class="mosaic-piece" src={image} alt="IMG1" />
       </div>
-    {/each}
+    {/each} -->
+
+    {#await get() then get}
+      {#each get as image}
+        <!-- {@debug image} -->
+        <div class="image-container">
+          <img class="mosaic-piece" src={image.path.default} alt={image.name} />
+        </div>
+      {/each}
+    {/await}
   </section>
 </body>
