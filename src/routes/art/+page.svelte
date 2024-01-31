7c0ceb9dc9f2a0d524b1be996504f2b14487d0b1
Tekatre s <6poules@gmail.com>
2024-01-31 16:21:41 -0500

    fix on art section try 1

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   const imageModules = import.meta.glob("$lib/images/artDisplay/*.(png|gif)");
+  console.log("imageModules", imageModules);
 </script>
 
 <svelte:head>
@@ -12,6 +13,7 @@
   <section class="images-mosaic">
     {#each Object.keys(imageModules) as image}
       <div class="image-container">
+        {@debug image}
         <img class="mosaic-piece" src={image} alt="IMG1" />
       </div>
     {/each}
